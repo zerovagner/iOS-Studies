5785ac78bc0c2cf1321fc8398c6efaf960fc96d2
ZeroVagner <zero.vagner@gmail.com>
2017-04-28 14:59:39 -0300

    Extended the form by adding Country and Zip Code

--- MiraclePills/MiraclePills/ViewController.swift
+++ MiraclePills/MiraclePills/ViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+	@IBOutlet weak var countryPicker: UIPickerView!
+	@IBOutlet weak var countryButton: UIButton!
 	@IBOutlet weak var statePicker: UIPickerView!
 	@IBOutlet weak var stateButton: UIButton!
 	
+	@IBOutlet weak var countryLabel: UILabel!
+	@IBOutlet weak var zipCodeLabel: UILabel!
+	@IBOutlet weak var zipCodeTextField: UITextField!
+	
 	let states = ["Alaska", "Arizona", "California", "New York", "New Jersey", "Oregon", "Washignton"]
+	let countries = ["Argentina", "Brazil", "Canada", "England", "United States"]
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		statePicker.delegate = self
 		statePicker.dataSource = self
+		countryPicker.delegate = self
+		countryPicker.dataSource = self
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -28,23 +37,59 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
 
 	@IBAction func stateButtonPressed(_ sender: UIButton) {
 		statePicker.isHidden = false
+		
+		countryLabel.isHidden = true
+		countryButton.isHidden = true
+		countryPicker.isHidden = true
+		zipCodeTextField.isHidden = true
+		zipCodeLabel.isHidden = true
 	}
 
+	@IBAction func countryButtonPressed(_ sender: UIButton) {
+		countryPicker.isHidden = false
+		
+		statePicker.isHidden = true
+		zipCodeLabel.isHidden = true
+		zipCodeTextField.isHidden = true
+	}
+	
 	func numberOfComponents(in pickerView: UIPickerView) -> Int {
 		return 1
 	}
 	
 	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-		return states.count
+		if pickerView == statePicker {
+			return states.count
+		} else {
+			return countries.count
+		}
 	}
 	
 	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-		return states[row]
+		if pickerView == statePicker {
+			return states[row]
+		} else {
+			return countries[row]
+		}
 	}
 	
 	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-		stateButton.setTitle(states[row], for: UIControlState.normal)
-		statePicker.isHidden = true
+		if pickerView == statePicker {
+			stateButton.setTitle(states[row], for: UIControlState.normal)
+			statePicker.isHidden = true
+			
+			countryLabel.isHidden = false
+			countryButton.isHidden = false
+			zipCodeTextField.isHidden = false
+			zipCodeLabel.isHidden = false
+		}
+		if pickerView == countryPicker {
+			countryButton.setTitle(countries[row], for: UIControlState.normal)
+			countryPicker.isHidden = true
+		
+			zipCodeLabel.isHidden = false
+			zipCodeTextField.isHidden = false
+		}
 	}
 }
 
