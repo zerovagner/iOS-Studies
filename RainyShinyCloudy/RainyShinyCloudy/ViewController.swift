44e551973014b723dd702e6d796fcc4e8f6d54ee
ZeroVagner <zero.vagner@gmail.com>
2017-05-15 15:12:41 -0300

    Created the UI and some table logic.

--- RainyShinyCloudy/RainyShinyCloudy/ViewController.swift
+++ RainyShinyCloudy/RainyShinyCloudy/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+	@IBOutlet weak var dateLabel: UILabel!
+	@IBOutlet weak var temperatureLabel: UILabel!
+	@IBOutlet weak var cityLabel: UILabel!
+	@IBOutlet weak var weatherLabel: UILabel!
+	@IBOutlet weak var weatherImage: UIImageView!
+	@IBOutlet weak var tableView: UITableView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		tableView.dataSource = self
+		tableView.delegate = self
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +28,17 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 6
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let tableCell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+		return tableCell
+	}
 }
 
