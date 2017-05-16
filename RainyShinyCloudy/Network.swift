fafe3051d32bb27bc955a25ad1a0a0aca4929038
ZeroVagner <zero.vagner@gmail.com>
2017-05-16 00:00:37 -0300

    Laid out the basic Weather class and network functions

---
+++ RainyShinyCloudy/Network.swift
@@ -0,0 +1,16 @@
+//
+//  Network.swift
+//  RainyShinyCloudy
+//
+//  Created by Vagner Oliveira on 5/15/17.
+//  Copyright © 2017 Vagner Oliveira. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+	let baseUrl = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=\(NSLocalizedString("api_key", comment: ""))"
+	func downloadCurrentWeatherData () {
+		
+	}
---
+++ RainyShinyCloudy/RainyShinyCloudy/Weather.swift
@@ -0,0 +1,18 @@
+//
+//  Weather.swift
+//  RainyShinyCloudy
+//
+//  Created by Vagner Oliveira on 5/15/17.
+//  Copyright © 2017 Vagner Oliveira. All rights reserved.
+//
+
+import Foundation
+
+class Weather {
+	private(set) var cityName: String!
+	private(set) var currentWeather: String!
+	private(set) var currentDate: String!
+	private(set) var currentTemperature: Double!
+	private(set) var maxTemperature: Double!
+	private(set) var minTemperature: Double!
+}
